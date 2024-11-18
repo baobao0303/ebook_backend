2f5553ade65a020b1fe4267b1cbdca170f69f7e5
Bảo Bảo <baobao@Baos-MacBook-Pro.local>
2024-11-18 23:33:43 +0700

    getting history for a book

--- src/controllers/history.ts
+++ src/controllers/history.ts
@@ -1,8 +1,14 @@
 import HistoryModel from "@/models/history";
 import { UpdateHistoryRequestHandler } from "@/types";
+import { sendErrorResponse } from "@/utils/helper";
+import { RequestHandler } from "express";
+import { isValidObjectId } from "mongoose";
 
-export const updateBookHistory: UpdateHistoryRequestHandler = async (req, res) => {
-  const { bookId, highlights, lastLocation } = req.body;
+export const updateBookHistory: UpdateHistoryRequestHandler = async (
+  req,
+  res
+) => {
+  const { bookId, highlights, lastLocation, remove } = req.body;
 
   let history = await HistoryModel.findOne({
     book: bookId,
@@ -18,10 +24,58 @@ export const updateBookHistory: UpdateHistoryRequestHandler = async (req, res) =
     });
   } else {
     if (lastLocation) history.lastLocation = lastLocation;
-    if (highlights?.length) history.highlights.push(...highlights);
+
+    // storing highlights
+    if (highlights?.length && !remove) history.highlights.push(...highlights);
+
+    // removing highlights
+    if (highlights?.length && remove) {
+      //   history.highlights = history.highlights.filter((item) => {
+      //     const highlight = highlights.find((h) => {
+      //       if (h.selection === item.selection) {
+      //         return h;
+      //       }
+      //     });
+      //     if (!highlight) return true;
+      //   });
+      history.highlights = history.highlights.filter(
+        (item) => !highlights.find((h) => h.selection === item.selection)
+      );
+    }
   }
 
   await history.save();
 
   res.send();
+};
+
+export const getBookHistory: RequestHandler = async (req, res) => {
+  const { bookId } = req.params;
+  if (!isValidObjectId(bookId))
+    return sendErrorResponse({
+      res,
+      message: "Invalid book id!",
+      status: 422,
+    });
+
+  const history = await HistoryModel.findOne({
+    book: bookId,
+    reader: req.user.id,
+  });
+  if (!history)
+    return sendErrorResponse({
+      res,
+      message: "Not Found!",
+      status: 404,
+    });
+
+  res.json({
+    history: {
+      lastLocation: history.lastLocation,
+      highlights: history.highlights.map((h) => ({
+        fill: h.fill,
+        selection: h.selection,
+      })),
+    },
+  });
 };
